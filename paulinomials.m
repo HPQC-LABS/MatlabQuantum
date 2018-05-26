4f96eec4ace2a28621d63d46b6a36b889f0d363a
Nike Dattani <dattani.nike@gmail.com>
2018-05-25 20:33:01 -0400

    paulinomials.m now working for any square matrix of length 2,3,4,6 or 9 (confirmed with random matrix examples for each size).

--- paulinomials.m
+++ paulinomials.m
@@ -1,56 +1,58 @@
-function [a, error]=paulinomials(H)
-%%
-m{1}=[0 1 0;...
-      1 0 0;...
-      0 0 0];
+function [c, error]=paulinomials(H)
 
-m{2}=[0 -1i 0;...
-      1i 0 0;...
-      0 0 0];
+sigma{1}=[0 1; 1 0];
+sigma{2}=[0 -1i; 1i 0];
+sigma{3}=[1 0 ; 0 -1];
+sigma{4}=eye(2);
 
-m{3}=[1  0 0;...
-      0 -1 0;...
-      0 0 0];
-
-m{4}=[0 0 1;...
-      0 0 0;...
-      1 0 0];
-
-m{5}=[0  0 -1i;...
-      0  0  0;...
-      1i 0  0];
-
-m{6}=[0 0 0;...
-      0 0 1;...
-      0 1 0];
-
-m{7}=[0  0  0;...
-      0  0 -1i;...
-      0 1i  0];
-
-m{8}=[1 0 0;...
-      0 1 0;...
-      0 0 -2]/sqrt(3);
-
+m{1}=[0 1 0;1 0 0;0 0 0];
+m{2}=[0 -1i 0;1i 0 0;0 0 0];
+m{3}=[1  0 0;0 -1 0;0 0 0];
+m{4}=[0 0 1;0 0 0;1 0 0];
+m{5}=[0  0 -1i;0  0  0; 1i 0  0];
+m{6}=[0 0 0;0 0 1;0 1 0];
+m{7}=[0  0  0;0  0 -1i;0 1i  0];
+m{8}=[1 0 0;0 1 0;0 0 -2]/sqrt(3);
 m{9}=eye(3);
 
-for i=1:length(m)
-    for j=1:length(m)
-        temp=kron(m{i},m{j});
-        basis(:,sub2ind([length(m),length(m)],i,j))=temp(:);
-        %c(i,j)=(1/4)*trace(kron(m{i},m{j})*H); % this doesn't work because the basis is not orthogonal.
-    end
+switch length(H)
+    case 2; for i=1:4; c(i)=(1/2)*trace(sigma{i}*H);end
+    case 3
+        for i=1:length(H)^2
+            basis(:,i)=m{i}(:);
+        end
+        c=basis\H(:);
+    case 4
+        for i=1:length(H)
+            for j=1:length(H)
+                c(i,j)=(1/4)*trace(kron(sigma{i},sigma{j})*H);
+            end
+        end
+    case 6
+        for i=1:4
+            for j=1:9
+                temp=kron(sigma{i},m{j});
+                basis(:,sub2ind([4,9],i,j))=temp(:);
+            end
+        end
+        c=basis\H(:);
+    case 9
+        for i=1:length(H)
+            for j=1:length(H)
+                temp=kron(m{i},m{j});
+                basis(:,sub2ind([length(m),length(m)],i,j))=temp(:);
+            end
+        end
+        c=basis\H(:);
 end
 
-a=basis\H(:);
-
-b=zeros(9);d=b;
-for i=1:9
-    for j=1:9
-        b=b+a(sub2ind([length(m),length(m)],i,j))*kron(m{i},m{j});
-%        d=d+c(i,j)*kron(m{i},m{j});
+error=0;
+if length(H)==6
+    t=zeros(length(H));
+    for i=1:4
+        for j=1:9
+            t=t+c(sub2ind([4,9],i,j))*kron(sigma{i},m{j});
+            error=H-t;
+        end
     end
 end
-
-error=H-b;
-%disp(H-d)
